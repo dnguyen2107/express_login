9d2804ca224b6ad0eabda1c0ce8ec26c8fc3c694
duynguyen <nguyenhd2107@gmail.com>
2014-04-10 21:20:59 +0700

    update

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
